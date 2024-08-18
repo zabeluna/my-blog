2dbe53d0f06650fd88fb4f60453ba430fff799ae
izabelle <personalife503@gmail.com>
2024-08-18 14:33:52 -0300

    feat

--- components/hero-section.tsx
+++ components/hero-section.tsx
@@ -27,26 +27,32 @@ const HeroSection = () => {
       </div>
       <ChevronDown className="mx-auto animate-bounce" />
 
-      <div className="flex-col gap-3 px-12 md:mb-16 order-last text-center">
+      <div className="flex-col gap-3 py-8 px-12 md:mb-16 order-last text-center">
         <h1 className="text-blue-300 leading-tight md:leading-none text-xl font-header">INICIANDO</h1>
           <h1 className="text-5xl leading-tight md:leading-none md:text-5xl font-header font-semibold">
             Quem sou eu?
           </h1>
         </div>
-        <div className="flex-col gap-3 px-12 md:mb-16 order-last text-left">
+        <div className="flex-col gap-3 px-72 py-20 md:mb-16 order-last text-left">
         <blockquote>
   <p>Olá! Sou uma estudante de Ciência da Computação</p>
   <p>apaixonada por segurança cibernética e desenvolvi-</p>
   <p>mento web. Minha jornada me permitiu desenvolver</p>
   <p>uma base sólida em ambas as áreas.</p>
   <div>
-    {/* add img */}
+    <div className="px-80">
+     <Image
+      src="/foto.png"
+      width={200}
+      height={500}
+      alt="Picture of the author"
+    /></div>
     
   </div>
 </blockquote>
        </div>
        {/* diminuir fonte */}
-       <div className="flex-col gap-3 px-12 md:mb-16 order-last text-center">
+       <div className="flex-col gap-3 px-12 py-32 md:mb-16 order-last text-center">
         <p className="text-blue-300 leading-tight md:leading-none text-lg font-header">Estou em busca de</p>
         <p className="text-blue-300 leading-tight md:leading-none text-lg font-header">desafios reais</p>
        </div>
@@ -59,6 +65,32 @@ const HeroSection = () => {
             Habilidades e Tecnologias
           </h1>
        </div>
+       <div>
+       <div>
+     <Image
+      src="/js.png"
+      width={120}
+      height={500}
+      alt="Picture of the author"
+    /></div>
+
+     <div>
+     <Image
+      src="/ts.png"
+      width={150}
+      height={500}
+      alt="Picture of the author"
+    /></div>
+
+<div>
+     <Image
+      src="/git.png"
+      width={130}
+      height={500}
+      alt="Picture of the author"
+    /></div>
+       </div>
+      
        <div className="flex-col gap-3 px-12 md:mb-16 order-last text-center">
        <blockquote>
   <p className="text-blue-300 leading-tight md:leading-none text-lg font-header">Sou uma estudante dedicada com</p>
@@ -66,18 +98,17 @@ const HeroSection = () => {
   <p className="text-blue-300 leading-tight md:leading-none text-lg font-header">cibernética e desenvolvimento web.</p>
 </blockquote>
        </div>
-       <div  className="flex-col gap-3 px-12 md:mb-16 order-last text-center">
+       <div  className="flex-col gap-3 px-12 py-28 md:mb-16 order-last text-center">
        <h1 className="text-blue-300 leading-tight md:leading-none text-xl font-header">CONQUISTAS</h1>
        <h1 className="text-5xl leading-tight md:leading-none md:text-5xl font-header font-semibold">
             Badges e Certificados
           </h1>
        </div>
        <div className="flex-col gap-3 px-12 md:mb-16 order-last text-right">
+        {/*rever escritores*/}
        <blockquote>
   <p>Quando não estou no computador, amo explorar</p>
-  <p>a natureza, cozinhar, dançar, ler (meus escritores</p>
-  <p>favoritos são Cesar Bravo e Stephen King) e</p>
-  <p>sentir músicas.</p>
+  <p>a natureza, cozinhar, dançar, ler e sentir músicas.</p>
        </blockquote>
        </div>
        
